8386bd3df430672136042be0e4c79eb734fbfc69
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2023-06-05 09:20:25 +0200

    First working version

--- src/DocumentLayoutWidget.jsx
+++ src/DocumentLayoutWidget.jsx
@@ -1,33 +1,58 @@
-import { createElement, useCallback } from "react";
-
-import { BadgeSample } from "./components/BadgeSample";
 import "./ui/DocumentLayoutWidget.css";
+import { createElement } from "react";
 
 export function DocumentLayoutWidget(props) {
-    const {
-        documentlayoutwidgetType,
-        documentlayoutwidgetValue,
-        valueAttribute,
-        onClickAction,
-        style,
-        bootstrapStyle
-    } = props;
-    const onClickHandler = useCallback(() => {
-        if (onClickAction && onClickAction.canExecute) {
-            onClickAction.execute();
-        }
-    }, [onClickAction]);
+    const { headerContent, bodyContent, footerContent, headerHeight, footerHeight } = props;
+
+    const className = "document-layout-widget " + props.class;
+
+    // Created based on this blog post:
+    // https://medium.com/@Idan_Co/the-ultimate-print-html-template-with-header-footer-568f415f6d2a
+
+    // The header/footer style only contains the height value.
+    const headerStyle = {
+        height: headerHeight + "px"
+    };
+
+    const footerStyle = {
+        height: footerHeight + "px"
+    };
 
     return (
-        <BadgeSample
-            type={documentlayoutwidgetType}
-            bootstrapStyle={bootstrapStyle}
-            className={props.class}
-            clickable={!!onClickAction}
-            defaultValue={documentlayoutwidgetValue ? documentlayoutwidgetValue : ""}
-            onClickAction={onClickHandler}
-            style={style}
-            value={valueAttribute ? valueAttribute.displayValue : ""}
-        />
+        <div className={className}>
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            <div className="header-space" style={headerStyle}>
+                                &nbsp;
+                            </div>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className="content">{bodyContent}</div>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            <div className="footer-space" style={footerStyle}>
+                                &nbsp;
+                            </div>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+            <div className="header" style={headerStyle}>
+                {headerContent}
+            </div>
+            <div className="footer" style={footerStyle}>
+                {footerContent}
+            </div>
+        </div>
     );
 }
