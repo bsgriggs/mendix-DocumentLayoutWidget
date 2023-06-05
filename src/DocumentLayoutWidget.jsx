178636913ff6ebb203f2b276209b96ee7c82a80e
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2023-06-05 09:28:49 +0200

    Added comments
    Added comments in case original blog post is no longer available

--- src/DocumentLayoutWidget.jsx
+++ src/DocumentLayoutWidget.jsx
@@ -9,6 +9,12 @@ export function DocumentLayoutWidget(props) {
     // Created based on this blog post:
     // https://medium.com/@Idan_Co/the-ultimate-print-html-template-with-header-footer-568f415f6d2a
 
+    // General idea is to use a table because the header and footer of a table will be repeated on every page.
+    // However, the table footer on the last page will be shown directly underneath the content.
+    // So a combination is used: A table to reserve space but not render any content.
+    // Render content in the reserved space using position fixed.
+    // Without reserving space using the table, the contents would overflow the header and footer.
+
     // The header/footer style only contains the height value.
     const headerStyle = {
         height: headerHeight + "px"
